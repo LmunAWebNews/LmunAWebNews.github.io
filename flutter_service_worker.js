f8f27da447df6b99973b66fe52d812e641b07299
LmunAWebNews <114307473+LmunAWebNews@users.noreply.github.com>
2022-09-25 21:21:43 +0200

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "bbb7a88eda8909d885f2acdec78fec14",
-"index.html": "d558a8ee81fd8ba3f560467c7c1b6234",
-"/": "d558a8ee81fd8ba3f560467c7c1b6234",
-"main.dart.js": "272da1fe77fbd35724231c4dbf7b6457",
+"index.html": "4b24abda0e90e34242fd2c20a3ec2063",
+"/": "4b24abda0e90e34242fd2c20a3ec2063",
+"main.dart.js": "1e972dccb50b6a6a1ed0019cb6aa6234",
 "flutter.js": "8ae00b472ec3937a5bee52055d6bc8b4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
